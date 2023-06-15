bd5e32969e412f2d3703d53f82d175d53a5f7894
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2023-06-15 19:14:06 +0530

    installed express js layouts

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
 //const expressLayouts = require('express-ejs-layouts');
-const db=require('./config/mongoose');
-const session = require('express-session');
-const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
-const MongoStore = require('connect-mongo')(session);
+const db = require("./config/mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo")(session);
 //const sassMiddleware = require('node-sass-middleware');
-
+const expressLayouts = require("express-ejs-layouts");
 //app.use(sassMiddleware)
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 //set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // mongo store is used to store the session cookie in the db
-app.use(session({
-    name:'codeial',
-    //TODO change the secret before deployment in production node
-    secret:'blahsomething',
-    saveUninitialized: false,
-    resave:false,
-    cookie:{
-        maxAge:(1000*60*100)
-    },
-    store:new MongoStore(
-        {
-            mongooseConnection:db,
-            autoRemove:'disabled'
-        },
-        function(err){
-            console.log(err || 'connect-mongodb setup ok');
-        }
-    )
-}));
+app.use(
+	session({
+		name: "codeial",
+		//TODO change the secret before deployment in production node
+		secret: "blahsomething",
+		saveUninitialized: false,
+		resave: false,
+		cookie: {
+			maxAge: 1000 * 60 * 100,
+		},
+		store: new MongoStore(
+			{
+				mongooseConnection: db,
+				autoRemove: "disabled",
+			},
+			function (err) {
+				console.log(err || "connect-mongodb setup ok");
+			}
+		),
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -47,11 +52,11 @@ app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
 //use express router
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
-    if (err) {
-        console.log('error is running', err);
-    }
-    console.log('yes express server is running', port);
+	if (err) {
+		console.log("error is running", err);
+	}
+	console.log("yes express server is running", port);
 });
